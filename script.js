86c2889f99c3ada2f9268023a94d4bb056b79f26
hermanti <34585858+ghermain1@users.noreply.github.com>
2024-10-27 13:22:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 window.onscroll = function() {
     let header = document.getElementById("header");
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-};
-window.onscroll = function() {
-    let header = document.getElementById("header");
     let footer = document.getElementById("footer");
     
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
